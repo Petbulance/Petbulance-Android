c839880e7559d61b6e2f57729bcff0771f3cf77b
siri-bot-ui-22 <siri-bot-ui-22@users.noreply.github.com>
2025-10-24 07:34:42 +0000

    Update design tokens

--- presentation/src/main/java/com/example/presentation/component/theme/GeneratedColors.kt
+++ presentation/src/main/java/com/example/presentation/component/theme/GeneratedColors.kt
@@ -1,2 +1,14 @@
-package com.example.presentation.component.theme
 
+
+// Do not edit directly, this file was auto-generated.
+
+
+
+package 
+
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.*
+
+object {
+
+}
