aa8c8ed4ab354fd328d28ed45e7290666aa5c10c
Sirius_Alpha <scgv2222@naver.com>
2025-11-03 22:21:55 +0900

    Fix - Design token parser
    - Parse Primitive colors from tokens.json
    - generateColors.kts to apply color tokens

---
+++ presentation/src/main/java/com/example/presentation/component/theme/PetbulancePrimitives.kt
@@ -0,0 +1,211 @@
+package com.example.presentation.component.theme
+
+import androidx.compose.ui.graphics.Color
+
+object PetbulancePrimitives {
+    object Base {
+        val white: Color = Color(0XFFFFFFFF)
+
+        val black: Color = Color(0XFF000000)
+    }
+
+    object Gray {
+        val p50: Color = Color(0XFFFAFAFA)
+
+        val p100: Color = Color(0XFFF5F5F5)
+
+        val p200: Color = Color(0XFFEEEEEE)
+
+        val p300: Color = Color(0XFFE0E0E0)
+
+        val p400: Color = Color(0XFFBDBDBD)
+
+        val p500: Color = Color(0XFF9E9E9E)
+
+        val p600: Color = Color(0XFF757575)
+
+        val p700: Color = Color(0XFF616161)
+
+        val p800: Color = Color(0XFF424242)
+
+        val p900: Color = Color(0XFF222222)
+
+        val p950: Color = Color(0XFF1E1E1E)
+    }
+
+    object Primary {
+        val p50: Color = Color(0XFFE7F9EF)
+
+        val p100: Color = Color(0XFFCFF2E0)
+
+        val p200: Color = Color(0XFFA2E6C3)
+
+        val p300: Color = Color(0XFF72D9A4)
+
+        val p400: Color = Color(0XFF46CE88)
+
+        val p500: Color = Color(0XFF2DA969)
+
+        val p600: Color = Color(0XFF248955)
+
+        val p700: Color = Color(0XFF1B653F)
+
+        val p800: Color = Color(0XFF12442B)
+
+        val p900: Color = Color(0XFF092014)
+    }
+
+    object Secondary {
+        val p50: Color = Color(0XFFFEFEF6)
+
+        val p100: Color = Color(0XFFFDFCE7)
+
+        val p200: Color = Color(0XFFFBF9D0)
+
+        val p300: Color = Color(0XFFFAF5B8)
+
+        val p400: Color = Color(0XFFF8F2A0)
+
+        val p500: Color = Color(0XFFF6EF89)
+
+        val p600: Color = Color(0XFFF1E541)
+
+        val p700: Color = Color(0XFFD5C910)
+
+        val p800: Color = Color(0XFF8E860B)
+
+        val p900: Color = Color(0XFF474305)
+    }
+
+    object Tertiary {
+        val p50: Color = Color(0XFFFEFBFA)
+
+        val p100: Color = Color(0XFFFDF4F1)
+
+        val p200: Color = Color(0XFFFCECE8)
+
+        val p300: Color = Color(0XFFFBE1DA)
+
+        val p400: Color = Color(0XFFF9D6CD)
+
+        val p500: Color = Color(0XFFF8CCC0)
+
+        val p600: Color = Color(0XFFEF8C71)
+
+        val p700: Color = Color(0XFFE74D23)
+
+        val p800: Color = Color(0XFFA13012)
+
+        val p900: Color = Color(0XFF4E1809)
+    }
+
+    object Quaternary {
+        val p50: Color = Color(0XFFE8EBED)
+
+        val p100: Color = Color(0XFFB9C0C7)
+
+        val p200: Color = Color(0XFF97A1AC)
+
+        val p300: Color = Color(0XFF677686)
+
+        val p400: Color = Color(0XFF495C6F)
+
+        val p500: Color = Color(0XFF1C334B)
+
+        val p600: Color = Color(0XFF192E44)
+
+        val p700: Color = Color(0XFF142435)
+
+        val p800: Color = Color(0XFF0F1C29)
+
+        val p900: Color = Color(0XFF0C1520)
+    }
+
+    object Positive {
+        val p50: Color = Color(0XFFE6FAEE)
+
+        val p100: Color = Color(0XFFD0F5E1)
+
+        val p200: Color = Color(0XFFA2ECC2)
+
+        val p300: Color = Color(0XFF73E2A4)
+
+        val p400: Color = Color(0XFF45D985)
+
+        val p500: Color = Color(0XFF27BE69)
+
+        val p600: Color = Color(0XFF1F9854)
+
+        val p700: Color = Color(0XFF17723F)
+
+        val p800: Color = Color(0XFF104C2A)
+
+        val p900: Color = Color(0XFF082615)
+    }
+
+    object Negative {
+        val p50: Color = Color(0XFFFEECEC)
+
+        val p100: Color = Color(0XFFFCD5D4)
+
+        val p200: Color = Color(0XFFF9AAAA)
+
+        val p300: Color = Color(0XFFF5807F)
+
+        val p400: Color = Color(0XFFF25554)
+
+        val p500: Color = Color(0XFFEF2B2A)
+
+        val p600: Color = Color(0XFFD11110)
+
+        val p700: Color = Color(0XFF9D0C0C)
+
+        val p800: Color = Color(0XFF680808)
+
+        val p900: Color = Color(0XFF340404)
+    }
+
+    object Warning {
+        val p50: Color = Color(0XFFFFF8E5)
+
+        val p100: Color = Color(0XFFFFF3D1)
+
+        val p200: Color = Color(0XFFFFE59E)
+
+        val p300: Color = Color(0XFFFFD970)
+
+        val p400: Color = Color(0XFFFFCB3D)
+
+        val p500: Color = Color(0XFFFFBF0F)
+
+        val p600: Color = Color(0XFFD69D00)
+
+        val p700: Color = Color(0XFFA37800)
+
+        val p800: Color = Color(0XFF6B4F00)
+
+        val p900: Color = Color(0XFF382900)
+    }
+
+    object Info {
+        val p50: Color = Color(0XFFE6F2FF)
+
+        val p100: Color = Color(0XFFCCE5FF)
+
+        val p200: Color = Color(0XFF9ACAFE)
+
+        val p300: Color = Color(0XFF67B0FE)
+
+        val p400: Color = Color(0XFF3A98FD)
+
+        val p500: Color = Color(0XFF067DFD)
+
+        val p600: Color = Color(0XFF0265CF)
+
+        val p700: Color = Color(0XFF014A98)
+
+        val p800: Color = Color(0XFF013165)
+
+        val p900: Color = Color(0XFF001933)
+    }
+}
